aeb9f11e11137d6a82d57f2a7bf9be850d1e0a3b
Ganilha <pedroganilha@gmail.com>
2023-08-05 02:22:41 +0100

    Optimize imports

--- components/CardioIDLogging/cidlogging.c
+++ components/CardioIDLogging/cidlogging.c
@@ -1,21 +1,11 @@
-#include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <sys/stat.h>
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_vfs_fat.h"
 #include "driver/sdmmc_host.h"
 #include "driver/sdspi_host.h"
 #include "sdmmc_cmd.h"
-#include <dirent.h>
-#include <dirent.h>
-
-#include "esp_wifi.h"
 #include "esp_event_loop.h"
-#include "nvs_flash.h"
-#include "esp_netif.h"
 
 #include "cidLogging.h"
 #include "ssh.h"
--- main/main.c
+++ main/main.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
-#include "esp_err.h"
-#include "esp_ipc.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 #include <unistd.h>
-#include <sys/stat.h>
 
 #include "cidlogging.h"
-
-#define TOTAL_CALL_AMOUNT 200
-#define PERFMON_TRACELEVEL -1 // -1 - will ignore trace level
 
 void app_main(void)
 {
 	CARDIO_LOGGING_INIT();
 
 	char *TAG = "LOG_TEST";
-	CARDIO_LOG(TAG, "Error Log", 0);
-	CARDIO_LOG(TAG, "Warning Log", 1);
-	CARDIO_LOG(TAG, "Information Log", 2);
-	CARDIO_LOG(TAG, "Debug Log", 3);
-	CARDIO_LOG(TAG, "Verbose Log", 4);
+	int count = 0;
+	int lower_bound = 10;
+	int upper_bound = 60;
+	CARDIO_LOG("STATUS", "SENSOR=ON", 2);
+	while (count < 10)
+	{
+		CARDIO_LOG(TAG, "Warning Log", 1);
 
+		int cpu = (rand() % (upper_bound - lower_bound + 1)) + lower_bound;
+		int cpu2 = (rand() % (upper_bound - lower_bound + 1)) + lower_bound;
+		char formatted_string[20]; // Adjust the size as needed based on your expected string length
+		sprintf(formatted_string, "CPU0=%d CPU1=%d", cpu, cpu2);
+		CARDIO_LOG("TEMPERATURE", formatted_string, 2);
+
+		cpu = (rand() % (upper_bound - lower_bound + 1)) + lower_bound;
+		sprintf(formatted_string, "SENSOR=%d", cpu);
+		CARDIO_LOG("TEMPERATURE", formatted_string, 2);
+
+		count++; // Increment the count by 1 in each iteration
+
+		sleep(10);
+	}
+	CARDIO_LOG("STATUS", "SENSOR=OFF", 2);
+	// CARDIO_LOG(TAG, "Error Log", 0);
 	SEND_LOG_OVER_SSH();
 
 	while (1)
